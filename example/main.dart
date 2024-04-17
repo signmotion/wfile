48d8c23aaa0801b38d57fb1651686148a0ee9baa
Andrii Syrokomskyi <signmotion@gmail.com>
2024-04-17 18:09:42 +0200

    Added Identify a type and Identify the MIME type.

--- example/main.dart
+++ example/main.dart
@@ -4,5 +4,13 @@ import 'package:wfile/wfile.dart';
 
 void main() {
   final f = WFile('example/data');
+
+  // read a content
   print(f.readAsText('happy.txt'));
+
+  // identiry a type
+  print(f.binary('image.webp'));
+
+  // identiry the MIME type
+  print(f.mime('image.webp'));
 }
