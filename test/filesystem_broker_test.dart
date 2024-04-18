1a3a3452cc12230b455b259cb4b5cfceaba713cf
Andrii Syrokomskyi <signmotion@gmail.com>
2024-04-18 07:28:50 +0200

    Renamed output folder.

--- test/filesystem_broker_test.dart
+++ test/filesystem_broker_test.dart
@@ -7,7 +7,7 @@ import 'package:wfile/wfile.dart';
 void main() {
   group('FilesystemBroker Text', () {
     final outputPath =
-        ph.join('test', 'output', 'filesystem_broker_test', 'text');
+        ph.join('test', '_output', 'filesystem_broker_test', 'text');
     final broker = TextFilesystemBroker(outputPath);
 
     const key = '1.txt';
